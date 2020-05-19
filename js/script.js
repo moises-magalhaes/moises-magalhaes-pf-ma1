ca79ac95ee9d9f593b40119fcb316154c8383ed0
moises-magalhaes <63241484+moises-magalhaes@users.noreply.github.com>
2020-05-19 11:45:10 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,34 @@
+//question 1;
+
+var firstVar;
+
+//question 2;
+var name;
+name = 'Moises';
+
+//question 3;
+
+var number = 30;
+
+//question 4;
+var division = 20 / 5;
+console.log(division);
+
+//question 5;
+var frog = 'animal';
+console.log(typeof frog);
+
+//question 6;
+var orderHasShipped = true;
+
+//question 7;
+if (orderHasShipped === true) {
+	console.log('true');
+} else {
+	console.log('false');
+}
+
+//question 8;
+for (var count = 1; count <= 9; count++) {
+	console.log(count);
+}
